cfd7a832e26749e382eee681a5c0fd8af7550293
Damir <efoger@gmail.com>
2025-10-26 06:12:49 +0300

    Initialize main # Conflicts: #	.github/workflows/shortenertest.yml #	README.md #	cmd/shortener/main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,25 +1,3 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/Foga2H/yandex-go-musthave-shortener/internal/handler"
-	storage "github.com/Foga2H/yandex-go-musthave-shortener/internal/storage/memory"
-)
-
-func main() {
-	mux := http.NewServeMux()
-	memStorage := storage.NewMemStorage()
-
-	mux.Handle(`/`, &handler.CreateLinkHandler{
-		Storage: memStorage,
-	})
-	mux.Handle(`/{url}`, &handler.LinkHandler{
-		Storage: memStorage,
-	})
-
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
-}
+func main() {}
